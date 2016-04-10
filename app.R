5af4120c3a25994184a07356467c901afe45e382
hailianghuang <hailianghuang@gmail.com>
2016-04-10 09:13:54 -0400

    Changed the x-axis for case/control to use relative risk

--- app.R
+++ app.R
@@ -26,7 +26,9 @@ ui <- shinyUI(fluidPage(
          numericInput("N_rep", "Number of replications", 5),
 #         ),
        numericInput("N", "Sample size", 5000),
-       numericInput("r", "Case:Control ratio", 1, min=0.1, max=10, step=1)),
+       numericInput("r", "Case:Control ratio", 1, min=0.1, max=10, step=1),
+       numericInput("R", "Maximum relative risk", 10, min=1.5, max=10)
+      ),
        actionButton("goButton", "Run analysis"),
 	     downloadButton("downloadPDF", "Save figure")
      ),
@@ -52,10 +54,11 @@ server <- shinyServer(function(input, output, session) {
              "Case/Control" = wellPanel(
                         numericInput("K", "Disease prevalence", 0.01, min=0.00001, max=0.2, step=0.01),
                         numericInput("s", "Number of variants in gene", 50, min=1, max=10000, step=1),                
+                        numericInput("f_gene", "Number of variants that are functional", 1, min=1, max=10000, step=1), 
                         numericInput("AF_bar", "Average minor allele frequency", 0.001, min=0.00001, max=0.05, step=0.001), 
-                        numericInput("R", "Relative risk for functional alleles", 1.5, min=1, max=20, step=0.5), 
                         numericInput("P_cut_single", "p-value threshold for single variant test", 1E-10, min=1E-12, max=0.05, step=1E-10),
-                        numericInput("p_cut_burden", "p-value threshold for burden test", 1E-10, min=1E-12, max=0.05, step=1E-10)
+                        numericInput("p_cut_burden", "p-value threshold for burden test", 1E-10, min=1E-12, max=0.05, step=1E-10),
+                        checkboxInput("showCI", "show 95% confidence interval", value=FALSE)
              ),
              "De novo" = wellPanel(
                         numericInput("denovo_genome", "Number of de novo mutations per genome", 60, min=10, max=200, step=5), 
@@ -92,8 +95,13 @@ server <- shinyServer(function(input, output, session) {
   
     plotRegular <- function(){
       
-      n_function <- c(1:input$s)
+#      n_function <- c(1:input$s)
       
+      if(input$f_gene > input$s){
+        plot(0, 0, type="n", xlab="",  ylab="", xlim=c(0,1), ylim=c(0,1), axes=F)
+        text(0,1, adj=0, "Number of functonal variant must be less than total number of variants.", col="red")
+        return()
+      }
       AF_raw <- rexp(input$s*input$N_rep, 1/input$AF_bar)
       AF_raw <- matrix(AF_raw, input$s, input$N_rep)
       
@@ -109,26 +117,48 @@ server <- shinyServer(function(input, output, session) {
       power_single <- c()
       sum_var <- apply(AF_save, 2, function(x){ sum(x*(1-x) ) })
       
+      R <- seq(1, input$R, by=0.5)
       withProgress(message = 'Making plot', value=0, {
         for (i_rep in c(1:input$N_rep)){
-          power_burden <- cbind(power_burden, unlist(lapply(n_function, getBurdenPower, AF_save[,i_rep], sum_var[i_rep], input$R, input$K, input$N, input$r, input$p_cut_burden )))
-          power_single <- cbind(power_single, unlist(lapply(n_function, getSingleVarPower, AF_save[,i_rep], input$R, input$K, input$N, input$r, input$P_cut_single, i_rep)))
+#          power_burden <- cbind(power_burden, unlist(lapply(n_function, getBurdenPower, AF_save[,i_rep], sum_var[i_rep], input$R, input$K, input$N, input$r, input$p_cut_burden )))
+#          power_single <- cbind(power_single, unlist(lapply(n_function, getSingleVarPower, AF_save[,i_rep], input$R, input$K, input$N, input$r, input$P_cut_single, i_rep)))
+          power_burden <- cbind(power_burden, unlist(lapply(R, function(R, f, AF, sum_var, K, N, r, p_cut_burden){getBurdenPower(f, AF, sum_var, R, K, N, r, p_cut_burden)}, input$f_gene, AF_save[,i_rep], sum_var[i_rep], input$K, input$N, input$r, input$p_cut_burden )))
+          power_single <- cbind(power_single, unlist(lapply(R, function(R, f, AF, K, N, r, P_cut_single, comment) {getSingleVarPower(f, AF, R, K, N, r, P_cut_single, comment)}, input$f_gene, AF_save[,i_rep],  input$K, input$N, input$r, input$P_cut_single, i_rep)))
         }
       })
       
-      plot(0, 0, type="n", xlab="# of function variants",  ylab="Power", xlim=range(n_function), ylim=c(0,1))
-      legend("topleft", c("Burden","Single Variant"), bty="n", lty=c("solid","dashed"), lwd=3, col=c("blue","red"))
-      for (i_rep in c(1:input$N_rep)){
-        lines(n_function, power_burden[, i_rep], lwd=3,col="blue")
-        lines(n_function, power_single[, i_rep], lwd=3, col="red", lty="dashed")
+      plot(0, 0, type="n", xlab="Relative risk",  ylab="Power", xlim=range(R), ylim=c(0,1))
+      legend("topleft", c("Burden","Single Variant"), bty="n", lty=c("solid","solid"), lwd=3, col=c("blue","red"))
+      
+      lines(R, apply(power_burden, 1, mean), lwd=3,col="blue")
+      lines(R, apply(power_single, 1, mean), lwd=3,col="red")
+      
+      if(input$showCI){
+        sd <-  apply(power_burden, 1, sd)/sqrt(input$N_rep) * 1.96
+        lines(R, pmin(apply(power_burden, 1, mean) +sd,1), lwd=1,col="blue", lty="dashed")
+        lines(R, pmax(apply(power_burden, 1, mean) -sd,0) , lwd=1,col="blue", lty="dashed")
+        sd <-  apply(power_single, 1, sd)/sqrt(input$N_rep) * 1.96
+        lines(R, pmin(apply(power_single, 1, mean) +sd, 1), lwd=1,col="red", lty="dashed")
+        lines(R, pmax(apply(power_single, 1, mean) -sd, 0), lwd=1,col="red", lty="dashed")
       }
+      
+#      for (i_rep in c(1:input$N_rep)){
+#        lines(n_function, power_burden[, i_rep], lwd=3,col="blue")
+#        lines(n_function, power_single[, i_rep], lwd=3, col="red", lty="dashed")
+#      }
       
     }
 
     plotDenovo <- function(){
       
-      plot(0, 0, type="n", xlab="Relative Risk", ylab="Power", xlim=c(1,10), ylim=c(0,1))
-      rr <- seq(1, 10, by=.5)
+      if(input$f_func_genome * input$denovo_genome < input$denovo_select * input$f_func_select ){
+        plot(0, 0, type="n", xlab="",  ylab="", xlim=c(0,1), ylim=c(0,1), axes=F)
+        text(0,1, adj=0, "Select a smaller proportion of functional alleles in selected regions", col="red")
+        return()
+      }
+      
+      plot(0, 0, type="n", xlab="Relative Risk", ylab="Power", xlim=c(1, input$R), ylim=c(0,1))
+      rr <- seq(1, input$R, by=.5)
       power_para_genome <- getDenovoPower_parametric(rr, input$denovo_genome, input$f_func_genome,  input$N, input$r, input$p_cut_burden.denovo)
       power_para_selected <- getDenovoPower_parametric(rr, input$denovo_select , input$f_func_select, input$N, input$r,  input$p_cut_burden.denovo)
       
